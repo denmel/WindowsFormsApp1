d85f575f05af16aa8f6a12a9cfce8adb1f29ab51
dsmirnov@mfmgutu.ru <dsmirnov@mfmgutu.ru>
2022-04-27 02:01:16 +0500

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -27,15 +27,89 @@ namespace WindowsFormsApp1
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
+        
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tlp = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlp
+            // 
+            this.tlp.ColumnCount = 4;
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlp.Location = new System.Drawing.Point(0, 24);
+            this.tlp.Name = "tlp";
+            this.tlp.RowCount = 4;
+            this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp.Size = new System.Drawing.Size(558, 489);
+            this.tlp.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(558, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(558, 513);
+            this.Controls.Add(this.tlp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TableLayoutPanel tlp;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.Button button16;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WindowsFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        Button[] buttons = new Button[16];
+        Random rnd = new Random();
+        TableLayoutPanelCellPosition space = new TableLayoutPanelCellPosition();
+
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 16; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Dock = DockStyle.Fill;
+                buttons[i].Font = new Font("Segoe UI Semibold", 35F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+                buttons[i].UseVisualStyleBackColor = true;
+                buttons[i].Click += new EventHandler(button1_Click);
+                tlp.Controls.Add(buttons[i],i%4,i/4);
+            }
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<int> n = new List<int>();
+            int x;
+            do
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    n.Add(i);
+                }
+                for (int i = 0; i < 16; i++)
+                {
+                    x = rnd.Next(n.Count);
+                    if (n[x] == 0)
+                    {
+                        (tlp.Controls[i] as Button).Text = "";
+                        space.Row = i / 4;
+                        space.Column = i % 4;
+                    }
+                    else (tlp.Controls[i] as Button).Text = n[x].ToString();
+                    n.RemoveAt(x);
+                }
+            } while (!isCorrect());
+        }
+
+        private bool isCorrect()
+        {
+            int s = space.Row+1;
+            for (int i = 0; i < 15; i++)
+            {
+                int c1 = ((tlp.Controls[i] as Button).Text == "" ? 0 : Int32.Parse((tlp.Controls[i] as Button).Text));
+                for (int j = i+1; i < 16; i++)
+                {
+                    int c2 = ((tlp.Controls[j] as Button).Text == "" ? 16 : Int32.Parse((tlp.Controls[j] as Button).Text));
+                    if (c1 > c2) s++;
+                }
+            }
+            return s % 2==1 ? false : true;
+        }
+
+        private bool canMove(TableLayoutPanelCellPosition position)
+        {
+            return Math.Abs(position.Row - space.Row) + Math.Abs(position.Column - space.Column) == 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TableLayoutPanelCellPosition position = tlp.GetCellPosition(sender as Control);
+
+            if (canMove(position))
+            {
+                (tlp.Controls[tlpToInt(space)] as Button).Text = (tlp.Controls[tlpToInt(position)] as Button).Text;
+                (tlp.Controls[tlpToInt(position)] as Button).Text = "";
+                Swap(ref position, ref space);
+                if (checkWin())
+                {
+                    MessageBox.Show("Win");
+                }
+            }
+        }
+        private void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        private int tlpToInt(TableLayoutPanelCellPosition position)
+        {
+            return position.Row * 4 + position.Column;
+        }
+
+        private bool checkWin()
+        {
+            foreach (Control control in tlp.Controls)
+            {
+                if (((control as Button).Text==""?0:Int32.Parse((control as Button).Text))!=(tlpToInt(tlp.GetCellPosition(control))+1)%16)
+                {
+                    return false;
+                }                
+            }
+            return true;
         }
     }
 }
